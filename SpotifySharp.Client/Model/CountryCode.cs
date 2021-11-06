0d0c25a7670550b25d4aa3da120b7d1050d6340f
M Emery <thepolytheist@users.noreply.github.com>
2021-11-06 11:06:18 -0400

    Add country codes model class

---
+++ SpotifySharp.Client/Model/CountryCode.cs
@@ -0,0 +1,283 @@
+﻿namespace SpotifySharp.Client.Model
+{
+    public class CountryCode
+    {
+        private CountryCode(string value) { Value = value; }
+
+        public string Value;
+
+        public override string ToString()
+        {
+            return Value;
+        }
+
+        public static CountryCode ANDORRA => new CountryCode("AD");
+        public static CountryCode UNITED_ARAB_EMIRATES => new CountryCode("AE");
+        public static CountryCode AFGHANISTAN => new CountryCode("AF");
+        public static CountryCode ANTIGUA_AND_BARBUDA => new CountryCode("AG");
+        public static CountryCode ANGUILLA => new CountryCode("AI");
+        public static CountryCode ALBANIA => new CountryCode("AL");
+        public static CountryCode ARMENIA => new CountryCode("AM");
+        public static CountryCode ANGOLA => new CountryCode("AO");
+        public static CountryCode ANTARCTICA => new CountryCode("AQ");
+        public static CountryCode ARGENTINA => new CountryCode("AR");
+        public static CountryCode AMERICAN_SAMOA => new CountryCode("AS");
+        public static CountryCode AUSTRIA => new CountryCode("AT");
+        public static CountryCode AUSTRALIA => new CountryCode("AU");
+        public static CountryCode ARUBA => new CountryCode("AW");
+        public static CountryCode ALAND_ISLANDS => new CountryCode("AX");
+        public static CountryCode AZERBAIJAN => new CountryCode("AZ");
+        public static CountryCode
+            BOSNIA_AND_HERZEGOVINA => new CountryCode("BA");
+        public static CountryCode BARBADOS => new CountryCode("BB");
+        public static CountryCode BANGLADESH => new CountryCode("BE");
+        public static CountryCode BELGIUM => new CountryCode("AF");
+        public static CountryCode BURKINA_FASO => new CountryCode("BF");
+        public static CountryCode BULGARIA => new CountryCode("BG");
+        public static CountryCode BAHRAIN => new CountryCode("BH");
+        public static CountryCode BURUNDI => new CountryCode("BI");
+        public static CountryCode BENIN => new CountryCode("BJ");
+        public static CountryCode SAINT_BARTHELEMY => new CountryCode("BL");
+        public static CountryCode BERMUDA => new CountryCode("BM");
+        public static CountryCode BRUNEI_DARUSSALAM => new CountryCode("BN");
+        public static CountryCode BOLIVIA => new CountryCode("BO");
+        public static CountryCode
+            BONAIRE_SINT_EUSTATIUS_AND_SABA => new CountryCode("BQ");
+        public static CountryCode BRAZIL => new CountryCode("BR");
+        public static CountryCode BAHAMAS => new CountryCode("BS");
+        public static CountryCode BHUTAN => new CountryCode("BT");
+        public static CountryCode BOUVET_ISLAND => new CountryCode("BV");
+        public static CountryCode BOTSWANA => new CountryCode("BW");
+        public static CountryCode BELARUS => new CountryCode("BY");
+        public static CountryCode BELIZE => new CountryCode("BZ");
+        public static CountryCode CANADA => new CountryCode("CA");
+        public static CountryCode COCOS_ISLANDS => new CountryCode("CC");
+        public static CountryCode
+            DEMOCRATIC_REPUBLIC_OF_THE_CONGO => new CountryCode("CD");
+        public static CountryCode
+            CENTRAL_AFRICAN_REPUBLIC => new CountryCode("CF");
+        public static CountryCode CONGO => new CountryCode("CG");
+        public static CountryCode SWITZERLAND => new CountryCode("CH");
+        public static CountryCode COTE_DIVOIRE => new CountryCode("CI");
+        public static CountryCode COOK_ISLANDS => new CountryCode("CK");
+        public static CountryCode CHILE => new CountryCode("CL");
+        public static CountryCode CAMEROON => new CountryCode("CM");
+        public static CountryCode CHINA => new CountryCode("CN");
+        public static CountryCode COLOMBIA => new CountryCode("CO");
+        public static CountryCode COSTA_RICA => new CountryCode("CR");
+        public static CountryCode CUBA => new CountryCode("CU");
+        public static CountryCode CABO_VERDE => new CountryCode("CV");
+        public static CountryCode CURACAO => new CountryCode("CW");
+        public static CountryCode CHRISTMAS_ISLAND => new CountryCode("CX");
+        public static CountryCode CYPRUS => new CountryCode("CY");
+        public static CountryCode CZECHIA => new CountryCode("CZ");
+        public static CountryCode GERMANY => new CountryCode("DE");
+        public static CountryCode DJIBOUTI => new CountryCode("DJ");
+        public static CountryCode DENMARK => new CountryCode("DK");
+        public static CountryCode DOMINICA => new CountryCode("DM");
+        public static CountryCode DOMINICAN_REPUBLIC => new CountryCode("DO");
+        public static CountryCode ALGERIA => new CountryCode("DZ");
+        public static CountryCode ECUADOR => new CountryCode("EC");
+        public static CountryCode ESTONIA => new CountryCode("EE");
+        public static CountryCode EGYPT => new CountryCode("EG");
+        public static CountryCode WESTERN_SAHARA => new CountryCode("EH");
+        public static CountryCode ERITREA => new CountryCode("ER");
+        public static CountryCode SPAIN => new CountryCode("ES");
+        public static CountryCode EHTIOPIA => new CountryCode("ET");
+        public static CountryCode FINLAND => new CountryCode("FI");
+        public static CountryCode FIJI => new CountryCode("FJ");
+        public static CountryCode FALKLAND_ISLANDS => new CountryCode("FK");
+        public static CountryCode MICRONESIA => new CountryCode("FM");
+        public static CountryCode FAROE_ISLANDS => new CountryCode("FO");
+        public static CountryCode FRANCE => new CountryCode("FR");
+        public static CountryCode GABON => new CountryCode("GA");
+        public static CountryCode
+            UNITED_KINGDOM_OF_GREAT_BRITAIN_AND_NORTHERN_IRELAND
+                => new CountryCode("GB");
+        public static CountryCode GRENADA => new CountryCode("GD");
+        public static CountryCode GEORGIA => new CountryCode("GE");
+        public static CountryCode FRENCH_GUIANA => new CountryCode("GF");
+        public static CountryCode GUERNSEY => new CountryCode("GG");
+        public static CountryCode GHANA => new CountryCode("GH");
+        public static CountryCode GIBRALTAR => new CountryCode("GI");
+        public static CountryCode GREENLAND => new CountryCode("GL");
+        public static CountryCode GAMBIA => new CountryCode("GM");
+        public static CountryCode GUINEA => new CountryCode("GN");
+        public static CountryCode GUADELOUPE => new CountryCode("GP");
+        public static CountryCode EQUATORIAL_GUINEA => new CountryCode("GQ");
+        public static CountryCode GREECE => new CountryCode("GR");
+        public static CountryCode
+            SOUTH_GEORGIA_AND_THE_SOUTH_SANDWICH_ISLANDS
+                => new CountryCode("GS");
+        public static CountryCode GUATEMALA => new CountryCode("GT");
+        public static CountryCode GUAM => new CountryCode("GU");
+        public static CountryCode GUINEA_BISSAU => new CountryCode("GW");
+        public static CountryCode GUYANA => new CountryCode("GY");
+        public static CountryCode HONG_KONG => new CountryCode("HK");
+        public static CountryCode
+            HEARD_ISLAND_AND_MCDONALD_ISLANDS => new CountryCode("HM");
+        public static CountryCode HONDURAS => new CountryCode("HN");
+        public static CountryCode CROATIA => new CountryCode("HR");
+        public static CountryCode HAITI => new CountryCode("HT");
+        public static CountryCode HUNGARY => new CountryCode("HU");
+        public static CountryCode INDONESIA => new CountryCode("ID");
+        public static CountryCode IRELAND => new CountryCode("IE");
+        public static CountryCode ISRAEL => new CountryCode("IL");
+        public static CountryCode ISLE_OF_MAN => new CountryCode("IM");
+        public static CountryCode INDIA => new CountryCode("IN");
+        public static CountryCode
+            BRITISH_INDIAN_OCEAN_TERRITORY => new CountryCode("IO");
+        public static CountryCode IRAQ => new CountryCode("IQ");
+        public static CountryCode IRAN => new CountryCode("IR");
+        public static CountryCode ICELAND => new CountryCode("IS");
+        public static CountryCode ITALY => new CountryCode("IT");
+        public static CountryCode JERSEY => new CountryCode("JE");
+        public static CountryCode JAMAICA => new CountryCode("JM");
+        public static CountryCode JORDAN => new CountryCode("JO");
+        public static CountryCode JAPAN => new CountryCode("JP");
+        public static CountryCode KENYA => new CountryCode("KE");
+        public static CountryCode KYRGYZSTAN => new CountryCode("KG");
+        public static CountryCode CAMBODIA => new CountryCode("KH");
+        public static CountryCode KIRIBATI => new CountryCode("KI");
+        public static CountryCode COMOROS => new CountryCode("KM");
+        public static CountryCode
+            SAINT_KITTS_AND_NEVIS => new CountryCode("KN");
+        public static CountryCode KOREA => new CountryCode("KP");
+        public static CountryCode REPUBLIC_OF_KOREA => new CountryCode("KR");
+        public static CountryCode KUWAIT => new CountryCode("KW");
+        public static CountryCode CAYMAN_ISLANDS => new CountryCode("KY");
+        public static CountryCode KAZAKHSTAN => new CountryCode("KZ");
+        public static CountryCode LAO_PEOPLES_DEMOCRATIC_REPUBLIC => new CountryCode("LA");
+        public static CountryCode LEBANON => new CountryCode("LB");
+        public static CountryCode SAINT_LUCIA => new CountryCode("LC");
+        public static CountryCode LIECHTENSTEIN => new CountryCode("LI");
+        public static CountryCode SRI_LANKA => new CountryCode("LK");
+        public static CountryCode LIBERIA => new CountryCode("LR");
+        public static CountryCode LESOTHO => new CountryCode("LS");
+        public static CountryCode LITHUANIA => new CountryCode("LT");
+        public static CountryCode LUXEMBOURG => new CountryCode("LU");
+        public static CountryCode LATVIA => new CountryCode("LV");
+        public static CountryCode LIBYA => new CountryCode("LY");
+        public static CountryCode MOROCCO => new CountryCode("MA");
+        public static CountryCode MONACO => new CountryCode("MC");
+        public static CountryCode MOLDOVA => new CountryCode("MD");
+        public static CountryCode MONTENEGRO => new CountryCode("ME");
+        public static CountryCode SAINT_MARTIN => new CountryCode("MF");
+        public static CountryCode MADAGASCAR => new CountryCode("MG");
+        public static CountryCode MARSHALL_ISLANDS => new CountryCode("MH");
+        public static CountryCode NORTH_MACEDONIA => new CountryCode("MK");
+        public static CountryCode MALI => new CountryCode("ML");
+        public static CountryCode MYANMAR => new CountryCode("MM");
+        public static CountryCode MONGOLIA => new CountryCode("MN");
+        public static CountryCode MACAO => new CountryCode("MO");
+        public static CountryCode
+            NORTHERN_MARIANA_ISLANDS => new CountryCode("MP");
+        public static CountryCode MARTINIQUE => new CountryCode("MQ");
+        public static CountryCode MAURITANIA => new CountryCode("MR");
+        public static CountryCode MONTSERRAT => new CountryCode("MS");
+        public static CountryCode MALTA => new CountryCode("MT");
+        public static CountryCode MAURITIUS => new CountryCode("MU");
+        public static CountryCode MALDIVES => new CountryCode("MV");
+        public static CountryCode MALAWI => new CountryCode("MW");
+        public static CountryCode MEXICO => new CountryCode("MX");
+        public static CountryCode MALAYSIA => new CountryCode("MY");
+        public static CountryCode MOZAMBIQUE => new CountryCode("MZ");
+        public static CountryCode NAMIBIA => new CountryCode("NA");
+        public static CountryCode NEW_CALEDONIA => new CountryCode("NC");
+        public static CountryCode NIGER => new CountryCode("NE");
+        public static CountryCode NORFOLK_ISLAND => new CountryCode("NF");
+        public static CountryCode NIGERIA => new CountryCode("NG");
+        public static CountryCode NICARAGUA => new CountryCode("NI");
+        public static CountryCode NETHERLANDS => new CountryCode("NL");
+        public static CountryCode NORWAY => new CountryCode("NO");
+        public static CountryCode NEPAL => new CountryCode("NP");
+        public static CountryCode NAURU => new CountryCode("NR");
+        public static CountryCode NIUE => new CountryCode("NU");
+        public static CountryCode NEW_ZEALAND => new CountryCode("NZ");
+        public static CountryCode OMAN => new CountryCode("OM");
+        public static CountryCode PANAMA => new CountryCode("PA");
+        public static CountryCode PERU => new CountryCode("PE");
+        public static CountryCode FRENCH_POLYNESIA => new CountryCode("PF");
+        public static CountryCode PAPUA_NEW_GUINEA => new CountryCode("PG");
+        public static CountryCode PHILIPPINES => new CountryCode("PH");
+        public static CountryCode PAKISTAN => new CountryCode("PK");
+        public static CountryCode POLAND => new CountryCode("PL");
+        public static CountryCode
+            SAINT_PIERRE_AND_MIQUELON => new CountryCode("PM");
+        public static CountryCode PITCAIRN => new CountryCode("PN");
+        public static CountryCode PUERTO_RICO => new CountryCode("PR");
+        public static CountryCode PALESTINE => new CountryCode("PS");
+        public static CountryCode PORTUGAL => new CountryCode("PT");
+        public static CountryCode PALAU => new CountryCode("PW");
+        public static CountryCode PARAGUAY => new CountryCode("PY");
+        public static CountryCode QATAR => new CountryCode("QA");
+        public static CountryCode REUNION => new CountryCode("RE");
+        public static CountryCode ROMANIA => new CountryCode("RO");
+        public static CountryCode SERBIA => new CountryCode("RS");
+        public static CountryCode RUSSIAN_FEDERATION => new CountryCode("RU");
+        public static CountryCode RWANDA => new CountryCode("RW");
+        public static CountryCode SAUDI_ARABIA => new CountryCode("SA");
+        public static CountryCode SOLOMON_ISLANDS => new CountryCode("SB");
+        public static CountryCode SEYCHELLES => new CountryCode("SC");
+        public static CountryCode SUDAN => new CountryCode("SD");
+        public static CountryCode SWEDEN => new CountryCode("SE");
+        public static CountryCode SINGAPORE => new CountryCode("SG");
+        public static CountryCode
+            SAINT_HELENA_ASCENSION_AND_TRISTAN_DE_CUNHA
+                => new CountryCode("SH");
+        public static CountryCode SLOVENIA => new CountryCode("SI");
+        public static CountryCode
+            SVALBARD_AND_JAN_MAYEN => new CountryCode("SJ");
+        public static CountryCode SLOVAKIA => new CountryCode("SK");
+        public static CountryCode SIERRA_LEONE => new CountryCode("SL");
+        public static CountryCode SAN_MARINO => new CountryCode("SM");
+        public static CountryCode SENEGAL => new CountryCode("SN");
+        public static CountryCode SOMALIA => new CountryCode("SO");
+        public static CountryCode SURINAME => new CountryCode("SR");
+        public static CountryCode SOUTH_SUDAN => new CountryCode("SS");
+        public static CountryCode
+            SAO_TOME_AND_PRINCIPE => new CountryCode("SS");
+        public static CountryCode EL_SALVADOR => new CountryCode("SV");
+        public static CountryCode SINT_MAARTEN => new CountryCode("SX");
+        public static CountryCode SYRIAN_ARAB_REPUBLIC => new CountryCode("SY");
+        public static CountryCode ESWATINI => new CountryCode("SZ");
+        public static CountryCode
+            TURKS_AND_CAICOS_ISLANDS => new CountryCode("TC");
+        public static CountryCode CHAD => new CountryCode("TD");
+        public static CountryCode
+            FRENCH_SOUTHERN_TERRITORIES => new CountryCode("TF");
+        public static CountryCode TOGO => new CountryCode("TG");
+        public static CountryCode THAILAND => new CountryCode("TH");
+        public static CountryCode TAJIKISTIAN => new CountryCode("TJ");
+        public static CountryCode TOKELAU => new CountryCode("TK");
+        public static CountryCode TIMOR_LESTE => new CountryCode("TL");
+        public static CountryCode TURKMENISTAN => new CountryCode("TM");
+        public static CountryCode TUNISIA => new CountryCode("TN");
+        public static CountryCode TONGA => new CountryCode("TO");
+        public static CountryCode TURKEY => new CountryCode("TR");
+        public static CountryCode TRINIDAD_AND_TOBAGO => new CountryCode("TT");
+        public static CountryCode TUVALU => new CountryCode("TV");
+        public static CountryCode TAIWAN => new CountryCode("TW");
+        public static CountryCode TANZANIA => new CountryCode("TZ");
+        public static CountryCode UKRAINE => new CountryCode("UA");
+        public static CountryCode UGANDA => new CountryCode("UG");
+        public static CountryCode UNITED_STATES_MINOR_OUTLYING_ISLANDS => new CountryCode("UM");
+        public static CountryCode UNITED_STATES_OF_AMERICA => new CountryCode("US");
+        public static CountryCode URUGUAY => new CountryCode("UY");
+        public static CountryCode UZBEKISTAN => new CountryCode("UZ");
+        public static CountryCode HOLY_SEE => new CountryCode("VA");
+        public static CountryCode SAINT_VINCENT_AND_THE_GRENADINES => new CountryCode("VC");
+        public static CountryCode VENEZUELA => new CountryCode("VE");
+        public static CountryCode VIRGIN_ISLANDS_BRITISH => new CountryCode("VG");
+        public static CountryCode VIRGIN_ISLANDS_US => new CountryCode("VI");
+        public static CountryCode VIET_NAM => new CountryCode("VN");
+        public static CountryCode VANUATU => new CountryCode("VU");
+        public static CountryCode WALLIS_AND_FUTUNA => new CountryCode("WF");
+        public static CountryCode SAMOA => new CountryCode("WS");
+        public static CountryCode YEMEN => new CountryCode("YE");
+        public static CountryCode MAYOTTE => new CountryCode("YT");
+        public static CountryCode SOUTH_AFRICA => new CountryCode("ZA");
+        public static CountryCode ZAMBIA => new CountryCode("ZM");
+        public static CountryCode ZIMBABWE => new CountryCode("ZW");
+    }
+}
